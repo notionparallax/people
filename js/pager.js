d8f723749e2ac40210a015f7804128f1ed988cd2
Ben Doherty <ben@notionparallax.co.uk>
2016-01-25 17:26:15 +1100

    put pages next to text

--- js/pager.js
+++ js/pager.js
@@ -1,9 +1,22 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  let imgNode = '<img class="page-scan" src="pageImg/Image_Page_013.png">';
-  let breaks = document.querySelectorAll('span.page-break');
-  for(let i in breaks){
-    console.log(breaks[i]);
+  if (window.matchMedia("(min-width: 400px)").matches) {
+    let breaks = document.querySelectorAll('span.page-break');
+    for (let i in breaks) {
+      let b = breaks[i];
+      let pageNumber = (parseInt(b.innerHTML, 10) + 1).toString();
+      if (pageNumber.length === 1) {
+        pageNumber = '00' + pageNumber;
+      } else if (pageNumber.length === 2) {
+        pageNumber = '0' + pageNumber;
+      }
+      let pageImg = document.createElement("img");
+      pageImg.setAttribute('class', 'page-scan');
+      pageImg.setAttribute('src', 'pageImg/p_' + pageNumber + '.png');
+      b.appendChild(pageImg);
+    }
+  }else{
+    console.log("if you had a bigger screen you'd be seeing scans of the book too." );
   }
 });
