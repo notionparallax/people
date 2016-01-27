6c6d4a09ec3b6202a77eeb2256e7b6e94700032b
Ben Doherty <ben@notionparallax.co.uk>
2016-01-27 20:48:42 +1100

    added as needed load, close #6

--- js/pager.js
+++ js/pager.js
@@ -1,24 +1,42 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(event) {
-  if (window.matchMedia("(min-width: 450px)").matches) {
-    let breaks = document.querySelectorAll('span.page-break');
-    for (let i in breaks) {
-      let b = breaks[i];
-      let pageNumber = (parseInt(b.innerHTML, 10) + 1).toString();
-      if (pageNumber.length === 1) {
-        pageNumber = '00' + pageNumber;
-      } else if (pageNumber.length === 2) {
-        pageNumber = '0' + pageNumber;
+
+    let breaks = Array.from( document.querySelectorAll('span.page-break') );
+
+    function checkBreaks() {
+      for (var i = breaks.length - 1; i >= 0; i--) {
+        isScrolledIntoView(breaks[i]);
       }
-      let filename = 'p_' + pageNumber + '.png';
-      let pageImg = document.createElement("img");
-      pageImg.setAttribute('class', 'page-scan');
-      pageImg.setAttribute('src', 'pageImg/' + filename);
-      b.appendChild(pageImg);
-      ga('send', 'scan-load', filename);
     }
-  }else{
-    console.log("if you had a bigger screen you'd be seeing scans of the book too." );
-  }
-});
+
+    function isScrolledIntoView(elem) {
+        let top = elem.getBoundingClientRect().top
+        if ((top > 0) && (top < window.innerHeight)) {
+          //console.log(elem, "in view");
+          let pageNumber = (parseInt(elem.innerHTML, 10) + 1).toString();
+          if (pageNumber.length === 1) {
+            pageNumber = '00' + pageNumber;
+          } else if (pageNumber.length === 2) {
+            pageNumber = '0' + pageNumber;
+          }
+          let filename = 'p_' + pageNumber + '.png';
+          let pageImg = document.createElement("img");
+          pageImg.setAttribute('class', 'page-scan');
+          pageImg.setAttribute('src', 'pageImg/' + filename);
+          elem.appendChild(pageImg);
+          ga('send', 'scan-load', filename);
+
+          //remove this guy from breaks so the image only loads once
+          breaks.splice(breaks.indexOf(elem), 1);
+        }
+    }
+
+  if (window.matchMedia("(min-width: 450px)").matches) {
+      document.addEventListener("scroll", checkBreaks, false);
+    } else {
+      console.log("if you had a bigger screen you'd be seeing scans of the book too.");
+    }
+
+
+  });
